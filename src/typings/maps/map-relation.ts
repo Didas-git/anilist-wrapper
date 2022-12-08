0e42555612fc04d2ac55edf0a28ff6361a7ac802
DidaS <didasoficial@gmail.com>
2022-12-08 00:28:57 +0000

    Fixed relation mapping type

--- src/typings/maps/map-relation.ts
+++ src/typings/maps/map-relation.ts
@@ -2,12 +2,25 @@ import { MediaEdge, PageInfo } from "../../connection";
 import { MediaQuery } from "../../media-query";
 import { ExtractMedia, ExtractMediaEdge, ExtractPageInfo } from "../extract";
 
-export type MapRelation<E extends MediaEdge, M extends MediaQuery, P extends PageInfo> = {
+export type MapRelation<E extends MediaEdge, M extends MediaQuery, P extends PageInfo, KE = ExtractMediaEdge<E>, KM = ExtractMedia<M>, KP = ExtractPageInfo<P>> = {
     relations: {
-        [K in "edges" | "nodes" | "pageInfo" as keyof ExtractMediaEdge<E> extends never ? Exclude<K, "edges"> : keyof ExtractMedia<M> extends never ? Exclude<K, "nodes"> : keyof ExtractPageInfo<P> extends never ? Exclude<K, "pageInfo"> : K]: K extends "edges"
-        ? Array<ExtractMediaEdge<E>>
+        [K in "edges" | "nodes" | "pageInfo" as keyof KE extends never
+        ? keyof KM extends never
+        ? keyof KP extends never
+        ? never
+        : Exclude<K, "edges" | "nodes">
+        : keyof KP extends never
+        ? Exclude<K, "edges" | "pageInfo">
+        : Exclude<K, "edges">
+        : keyof KM extends never
+        ? keyof KP extends never ? Exclude<K, "nodes" | "pageInfo"> : Exclude<K, "nodes">
+        : keyof KP extends never
+        ? Exclude<K, "pageInfo">
+        : K
+        ]: K extends "edges"
+        ? Array<KE>
         : K extends "nodes"
-        ? Array<ExtractMedia<M>>
-        : ExtractPageInfo<P>
+        ? Array<KM>
+        : KP
     }
 }
