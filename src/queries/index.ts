79fce786dbc847bad15d4396c6b007ad0d9b5e9d
DidaS <didasoficial@gmail.com>
2023-07-17 14:49:22 +0100

    Fixed export order
    WTF NODE

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,6 +1,6 @@
+export * from "./query";
 export * from "./character-query";
 export * from "./studio-query";
 export * from "./media-query";
 export * from "./staff-query";
 export * from "./page-query";
-export * from "./query";
