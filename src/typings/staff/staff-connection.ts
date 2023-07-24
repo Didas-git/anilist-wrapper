e95847f43ce569065b3d25869471efccd723d889
DidaS <didasoficial@gmail.com>
2023-07-24 17:55:17 +0100

    Updated edge type name

--- src/typings/staff/staff-connection.ts
+++ src/typings/staff/staff-connection.ts
@@ -1,4 +1,4 @@
 import type { Connection } from "../connection";
-import type { Staff, StaffEdge } from ".";
+import type { Staff, IStaffEdge } from ".";
 
-export interface StaffConnection extends Connection<StaffEdge, Staff> { }
+export interface StaffConnection extends Connection<IStaffEdge, Staff> { }
